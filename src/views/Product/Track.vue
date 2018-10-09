a943e17588a10af02532e4ff1370a5ffd669e7fe
zhangyao <823237853@qq.com>
2018-10-09 14:01:06 +0800

    feat: 产品-策略跟踪 filter

--- src/views/Product/Track.vue
+++ src/views/Product/Track.vue
@@ -4,14 +4,14 @@
             <div class="title">策略跟踪和策略应用</div>
             <el-table :data="tableData.slice((currentPage - 1) * 10, currentPage * 10)" stripe>
                 <el-table-column type="index" label="序号"></el-table-column>
-                <el-table-column prop="a" label="产品层级" :filters="[{text: '品牌', value: '品牌'}]" :filter-method="filterA"></el-table-column>
-                <el-table-column prop="b" label="指标" :filters="[{text: '产品成本额', value: '产品成本额'}]" :filter-method="filterB"></el-table-column>
-                <el-table-column prop="c" label="评选等级" :filters="[{text: '差', value: '差'}]" :filter-method="filterC"></el-table-column>
-                <el-table-column prop="d" label="影响因素" :filters="[{text: '供应商', value: '供应商'}]" :filter-method="filterD"></el-table-column>
+                <el-table-column prop="a" label="产品层级" :filters="[{text: '品类', value: '品类'}]" :filter-method="filterA"></el-table-column>
+                <el-table-column prop="b" label="指标" :filters="[{text: '产品成本', value: '产品成本'}, {text: '日销', value: '日销'}]" :filter-method="filterB"></el-table-column>
+                <el-table-column prop="c" label="评选等级" :filters="[{text: '优', value: '优'},{text: '良', value: '良'},{text: '中', value: '中'},{text: '差', value: '差'}]" :filter-method="filterC"></el-table-column>
+                <el-table-column prop="d" label="影响因素" :filters="[{text: '供应商', value: '供应商'},{text: '采购', value: '采购'},{text: '预算', value: '预算'},{text: '包装', value: '包装'},{text: '摄影', value: '摄影'},{text: '流量', value: '流量'},{text: '转化', value: '转化'},{text: '客单', value: '客单'}]" :filter-method="filterD"></el-table-column>
                 <el-table-column prop="e" label="策略"></el-table-column>
-                <el-table-column prop="f" label="采纳次数" :filters="[{text: '4', value: '4'},{text: '3', value: '3'},{text: '2', value: '2'},{text: '1', value: '1'}]" :filter-method="filterF"></el-table-column>
-                <el-table-column prop="g" label="有效次数" :filters="[{text: '4', value: '4'},{text: '3', value: '3'},{text: '2', value: '2'},{text: '1', value: '1'}]" :filter-method="filterG"></el-table-column>
-                <el-table-column prop="h" label="策略准确度/适用度" :filters="[{text: '100%', value: '100%'}]" :filter-method="filterH"></el-table-column>
+                <el-table-column prop="f" label="采纳次数" :filters="[{text: '30', value: '30'},{text: '21', value: '21'},{text: '20', value: '20'},{text: '16', value: '16'},{text: '15', value: '15'},{text: '12', value: '12'},{text: '10', value: '10'},{text: '8', value: '8'},{text: '7', value: '7'},{text: '6', value: '6'}]" :filter-method="filterF"></el-table-column>
+                <el-table-column prop="g" label="有效次数" :filters="[{text: '19', value: '19'},{text: '18', value: '18'},{text: '14', value: '14'},{text: '12', value: '12'},{text: '10', value: '10'},{text: '9', value: '9'},{text: '8', value: '8'},{text: '7', value: '7'},{text: '5', value: '5'},{text: '4', value: '4'},{text: '3', value: '3'},]" :filter-method="filterG"></el-table-column>
+                <el-table-column prop="h" label="策略准确度/适用度" :filters="[{text: '100%', value: '100%'},{text: '95%', value: '95%'},{text: '93%', value: '93%'},{text: '90%', value: '90%'},{text: '87%', value: '87%'},{text: '83%', value: '83%'},{text: '75%', value: '75%'},{text: '67%', value: '67%'},{text: '60%', value: '60%'},{text: '50%', value: '50%'},]" :filter-method="filterH"></el-table-column>
             </el-table>
             <div class="page_container">
                 <el-pagination
