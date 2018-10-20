3d3ae106c856cb1a1341aabcb5705983af2ac0a9
chunxiao <15537791297@163.com>
2018-10-20 18:54:51 +0800

    10.18 产品对比

--- src/components/ConOrgComparisonAverageBig.vue
+++ src/components/ConOrgComparisonAverageBig.vue
@@ -6,112 +6,95 @@
 </template>
 
 <script>
-import echarts from 'echarts';
+    import echarts from 'echarts';
 
-export default {
-    props: {
-        id: String,
-        data: Object,
-        title: String,
-        index: Number
-    },
-    mounted() {
-        this.chart = echarts.init(document.getElementById(`ConOrgComparisonAverage-${this.id}`));
-        this.renderChart(this.data);
-    },
-    watch: {
-        data: {
-            handler: function (val, oldVal) {
-                this.renderChart(val);
-            },
-            deep: true
+    export default {
+        props: {
+            id: String,
+            data: Object,
+            title: String,
+            index: Number
         },
-    },
-    methods: {
-        renderChart(data) {
-            let _this = this;
-            const options = {
-                grid: {
-                    left: 0,
-                    right: 10,
-                    bottom: 0,
-                    top: 10,
-                    containLabel: true
+        mounted() {
+            this.chart = echarts.init(document.getElementById(`ConOrgComparisonAverage-${this.id}`));
+            this.renderChart(this.data);
+        },
+        watch: {
+            data: {
+                handler: function(val, oldVal) {
+                    this.renderChart(val);
                 },
-                // color: ['#D53A35', '#E98F6F', '#6AB0B8', '#334B5C'],
-                //title: {
-                //    text: '报警次数'
-                //},
-                tooltip: {
-                    trigger: 'axis',
-                    //formatter: "{b} <br> 合格率: {c}%"
-                },
-                // legend: {
-                //     data: ['旅游运输', '班线运输', '危险品', '普货']
-                // },
-                // toolbox: {
-                //     feature: {
-                //         saveAsImage: {}
-                //     }
-                // },
-                xAxis: {
-                    type: 'category',
-                    name: '日期',
-                    boundaryGap: false,
-                    data: ['5.1', '5.2', '5.3', '5.4', '5.5', '5.6', '5.7']
-                },
-                yAxis: {
-                    type: 'value',
-                    axisLabel: {
-                        formatter: _.includes([0,1,2,5,6], _this.index) ? '{value}' : '{value} %'
-                    }
-                    // name: '报警次数',
-                },
-                series: [{
-                        name: '旅游运输',
-                        type: 'line',
-                        stack: '总量',
-                        data: data.series[0]
+                deep: true
+            },
+        },
+        methods: {
+            renderChart(data) {
+                let _this = this;
+                const options = {
+                    grid: {
+                        left: 0,
+                        right: 10,
+                        bottom: 0,
+                        top: 10,
+                        containLabel: true
                     },
-                    {
-                        name: '班线运输',
-                        type: 'line',
-                        stack: '总量',
-                        data: data.series[1]
+                    // color: ['#D53A35', '#E98F6F', '#6AB0B8', '#334B5C'],
+                    //title: {
+                    //    text: '报警次数'
+                    //},
+                    tooltip: {
+                        trigger: 'axis',
+                        //formatter: "{b} <br> 合格率: {c}%"
                     },
-                    {
-                        name: '危险品',
-                        type: 'line',
-                        stack: '总量',
-                        data: data.series[2]
+                    // legend: {
+                    //     data: ['旅游运输', '班线运输', '危险品', '普货']
+                    // },
+                    // toolbox: {
+                    //     feature: {
+                    //         saveAsImage: {}
+                    //     }
+                    // },
+                    xAxis: {
+                        type: 'category',
+                        name: '日期',
+                        boundaryGap: false,
+                        data: ['5.1', '5.2', '5.3', '5.4', '5.5', '5.6', '5.7']
                     },
-                    {
-                        name: '普货',
-                        type: 'line',
-                        stack: '总量',
-                        data: data.series[3]
+                    yAxis: {
+                        type: 'value',
+                        axisLabel: {
+                            formatter: _.includes([0, 1, 2, 5, 6], _this.index) ? '{value}' : '{value} %'
+                        }
+                        // name: '报警次数',
                     },
-                ]
-            };
-            this.chart.setOption(options);
+                    series: []
+                };
+                for(let i = 0; i < data.series.length; i++) {
+                    options.series.push({
+                        name: this.data.subject_name,
+                        type: 'line',
+                        stack: i,
+                        data: data.series[i]
+                    });
+                }
+                this.chart.setOption(options);
+            }
         }
     }
-}
 </script>
 
 <style lang="scss" scoped>
-.ConOrgComparisonAverage-container {
-    .ConOrgComparisonAverage {
-        width: 840px;
-        height: 560px;
-        margin: 0 auto;
+    .ConOrgComparisonAverage-container {
+        .ConOrgComparisonAverage {
+            width: 840px;
+            height: 560px;
+            margin: 0 auto;
+        }
+        .detail {
+            text-align: center;
+            color: #5e5e5e;
+            font-size: 15px;
+            padding: 10px;
+        }
     }
-    .detail {
-        text-align: center;
-        color: #5e5e5e;
-        font-size: 15px;
-        padding: 10px;
-    }
-}
 </style>
-
