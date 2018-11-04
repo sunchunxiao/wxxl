2493437d20a11a83e9054fa7bcd067308bc4270b
zhangyao <823237853@qq.com>
2018-11-04 17:45:44 +0800

    feat(orgContrast): 产品对比交互

--- src/views/Product/Contrast.vue
+++ src/views/Product/Contrast.vue
@@ -17,18 +17,19 @@
           <span class="left">{{ productTree.name }}</span>
           <span class="right">{{ calculatePercent(productTree.real_total, productTree.target_total).percent + '%' }}</span>
         </div>
-        <!-- 有多个tree -->
+        <el-button 
+          @click="cleanChecked"
+          size="mini" 
+          v-if="cidObjArr.length > 0"
+          class="clean_btn">清空选择</el-button>
         <el-tree 
           :data="productTree.children" 
           ref="tree" 
           check-strictly
           node-key="cid" 
-          :default-expanded-keys="nodeArr"
           :props="defaultProps" 
-          @node-click="handleNodeClick" 
           show-checkbox 
-          @check-change="handleCheckChange" 
-          :default-checked-keys="setCheckedKeys">
+          @check-change="handleCheckChange">
           <span 
             class="custom-tree-node" 
             slot-scope="{ node, data }">
@@ -38,40 +39,41 @@
               :class="{progress: true, 'border-radius0': calculatePercent(data.real_total, data.target_total).largerThanOne}" 
               :style="{width: calculatePercent(data.real_total, data.target_total).largerThanOne ? '105%' : `${calculatePercent(data.real_total, data.target_total).percent + 5}%`}"/>
           </span>
-                    
         </el-tree>
       </el-col>
       <el-col 
         :span="19" 
         class="overflow">
-        <el-row v-loading="loading">
-          <Card>
-            <el-row class="card-title">产品对比分析和平均值分析</el-row>
-            <el-row>
-              <el-col :span="6">
-                <template v-for="(item, index) in compareArr">
-                  <el-col 
-                    :key="index" 
-                    :span="12" 
-                    @click.native="clickIndex(0 ,index)">
-                    <ConOrgComparisonAverage 
-                      :id="`${index}`" 
-                      :data="item"/>
-                  </el-col>
-                </template>
-              </el-col>
-              <el-col 
-                :span="18" 
-                v-if="compareArr.length > 0">
-                <ConOrgComparisonAverageBig 
-                  :title="pieData[index0].text" 
-                  :data="compareArr[index0]" 
-                  id="ConOrgComparisonAverage" 
-                  :index="index0"/>
-              </el-col>
-            </el-row>
-          </Card>
-        </el-row>
+        <Card>
+          <el-row class="card-title">产品对比分析和平均值分析</el-row>
+          <el-row v-if="compareArr.length > 0">
+            <el-col :span="6">
+              <template v-for="(item, index) in compareArr">
+                <el-col 
+                  :key="index" 
+                  :span="12" 
+                  @click.native="clickIndex(0 ,index)">
+                  <ConOrgComparisonAverage 
+                    :id="`${index}`" 
+                    :data="item"/>
+                </el-col>
+              </template>
+            </el-col>
+            <el-col 
+              :span="18">
+              <ConOrgComparisonAverageBig 
+                :title="compareArr[index0].subject_name" 
+                :data="compareArr[index0]" 
+                id="ConOrgComparisonAverage" 
+                :index="index0"/>
+            </el-col>
+          </el-row>
+          <el-row 
+            v-else 
+            class="please_select">
+            请选择要对比的项目
+          </el-row>
+        </Card>
       </el-col>
     </el-row>
   </div>
@@ -85,8 +87,6 @@
     import ConOrgComparisonAverage from '../../components/ConOrgComparisonAverage';
     import ConOrgComparisonAverageBig from '../../components/ConOrgComparisonAverageBig';
 
-    import mockPieData from './mock/pieData.js';
-    import mockComparisonAverageData from './mock/comparisonAverageData.js';
     import tree from './mock/productTreeData.js';
     import { mapGetters } from 'vuex';
 
@@ -94,12 +94,8 @@
         children: 'children',
         label: 'name'
     };
-    // const TIMEPT = {
-    //     '周': 'week',
-    //     '月': 'month',
-    //     '季': 'quarter',
-    //     '年': 'year'
-    // };
+    const ROOTCID = 1;
+    const SUBJECT = 'S'; // S: 销售额 P: 利润额
 
     export default {
         components: {
@@ -110,28 +106,12 @@
         },
         data() {
             return {
-                form: {
-                    pt: '日',
-                    date: [],
-                    search: '',
-                    subject: 'S', // S: 销售额 P: 利润额
-                    version: '0'
-                },
-                cid: 1,
-                loading: false,
                 tree: tree,
                 treeData: tree.data.children,
                 defaultProps: TREE_PROPS,
-                pieData: mockPieData(),
-                comparisonAverageData: mockComparisonAverageData(),
                 index0: 0,
-                series: [],
-                setCheckedKeys: [],
-                cidArr:[],
-                id:[10,20,30],
-                val:{},
-                post:1,
-                nodeArr:[]
+                cidObjArr:[],
+                cancelKey: ''
             };
         },
         computed: {
@@ -141,140 +121,120 @@
             }
         },
         watch: {
-            form: {
-                handler: function() {},
-                deep: true
-            },
-            cid: function() {
-                // 点击左侧树节点时, 请求右侧数据 看下是在点击树节点的时候做还是在这里做
-                // 暂时先在这里做
-                this.getProgress();
+            cidObjArr(val) {
+                if (val.length > 0) {
+                    const throttle = _.throttle(this.getCompare, 500);
+                    throttle();
+                } else if (val.length === 0) {
+                    this.$store.dispatch('ClearCompareArr');
+                }
             }
-        },
-        
+        },   
         mounted() {
-            //          console.log(this.hasTree)
-            if(!this.hasTree) {
-                this.getTree();
-            }
-            this.getProgress();
+            Promise.all([this.getTree(), this.getProgress()]).then(res => {
+                // 树
+                const treeData = res[0];
+                const children = treeData.tree.children;
+                let arr = [];
+                for(let i = 0; i < 3; i++) {
+                    children[i] && arr.push(children[i]);
+                }
+                this.cidObjArr = arr;
+                const checkKeys = this.cidObjArr.map(i => i.cid);
+                this.$refs.tree.setCheckedKeys(checkKeys);
+                this.$store.dispatch('SaveProductTree', treeData.tree);
+                // 指标
+                const progressData = res[1];
+                this.$store.dispatch('SaveProgressData', progressData.data);
+            });
         },
+
         methods: {
             getTree() {
                 const params = {
-                    // pt: this.form.pt,
-                    subject: this.form.subject,
+                    subject: SUBJECT,
                     ...this.getPeriodByPt(),
                 };
-                API.GetProductTree(params).then(res => {
-                    this.cidArr = [];
-                    let data = res.tree.children;
-                   
-                    for(let i=0;i<3;i++) {
-                        this.cidArr.push(data[i].cid);
-                    }
-                    // this.$refs.tree.setCheckedKeys(this.cidArr);
-                    this.$store.dispatch('SaveProductTree', res.tree);
-                });
+                return API.GetProductTree(params);
             },
             getProgress() {
                 const params = {
-                    cid: this.cid,
+                    cid: ROOTCID,
                     ...this.getPeriodByPt(),
                 };
-                API.GetProductProgress(params).then(res => {
-                    this.$store.dispatch('SaveProgressData', res.data);
-                    const promises = _.map(res.data, o => this.getTrend(o.subject));
-                    Promise.all(promises).then(resultList => {
-                        _.forEach(resultList, (v, k) => {
-                            v.subject = res.data[k].subject;
-                            v.subject_name = res.data[k].subject_name;
-                        });
-                        this.$store.dispatch('SaveCompareArr', resultList);
+                return API.GetProductProgress(params);
+            },
+            getCompare() {
+                const promises = _.map(this.progressArr, o => this.getTrend(o.subject));
+                Promise.all(promises).then(resultList => {
+                    _.forEach(resultList, (v, k) => {
+                        v.subject = this.progressArr[k].subject;
+                        v.subject_name = this.progressArr[k].subject_name;
                     });
+                    const cidName = this.cidObjArr.map(o => o.name);
+                    // 只有当返回的跟当前选中的一样才更新 store
+                    if(resultList[0] && resultList[0].nodes && _.isEqual(cidName, resultList[0].nodes)) {
+                        this.$store.dispatch('SaveCompareArr', resultList);
+                    }
                 });
             },
             getTrend(subject) {
-                const params = {
-                    targets:this.id.join(),
+                let params = {
                     ...this.getPeriodByPt(),
                     subject: subject
                 };
+                const checkKeys = this.cidObjArr.map(i => i.cid);
+                params.targets = checkKeys.join(',');
                 return API.GetProductCompare(params);
             },
             getPeriodByPt() {
-                const {
-                    sDate,
-                    eDate
-                } = this.getDateObj();
-                // const {
-                //     pt
-                // } = this.form;
-                // console.log(sDate,eDate);
-                if(sDate && eDate) { // 计算时间周期
-                        return {
-                            pt:this.val.pt,
-                            sDate: this.val.sDate,
-                            eDate: this.val.eDate,
-                            
-                        };
-                   
-                } else {
-                    return {
-                        pt:'日',
-                        sDate: '2018-01-01',
-                        eDate: '2018-01-07',
-                        // 先写死个时间
-                        // sDate: moment().startOf('week').format('YYYY-MM-DD'),
-                        // eDate: moment().format('YYYY-MM-DD'),
-                    };
-                }
-            },
-            getDateObj() {
-                const {
-                    date
-                } = this.form;
-                if(this.val.sDate!=undefined&&this.val.eDate!=undefined){
-                    return {
-                    sDate: this.val.sDate,
-                    eDate: this.val.eDate,
+                // 先写死个时间
+                return {
+                    pt:'日',
+                    sDate: '2018-01-01',
+                    eDate: '2018-01-07',
                 };
-                }else{
-                    return {
-                    sDate: date[0] || '',
-                    eDate: date[1] || '',
-                };
+            },
+            handleSearch() {
+            },
+            cleanChecked() {
+                this.cidObjArr = [];
+                this.$refs.tree.setCheckedKeys([]);
+            },
+            handleCheckChange(data, checked) {
+                // 取消选择多于 4 个的后面的值 这个是为了在 setCheckedKeys 时, 第四个以后的都会取消选择
+                if(!checked && this.cancelKey && data.cid === this.cancelKey) {
+                    return;
+                }
+                if (checked) { // 如果选中
+                    // 如果有选中的节点 并且此次选择了不同pid的节点
+                    if (this.cidObjArr[0] && data.parent_id !== this.cidObjArr[0].parent_id) {
+                        this.warn('请选择相同父级下的进行对比');
+                        this.cancelKey = data.cid;
+                        const checkKeys = this.cidObjArr.map(i => i.cid);
+                        this.$refs.tree.setCheckedKeys(checkKeys);
+                        return;
+                    }
+                    // 如果选中的个数不超过 4
+                    if (this.cidObjArr.length < 4) {
+                        this.cidObjArr.push(data);
+                    } else if (this.cidObjArr.length === 4) {
+                        this.warn('最多对比 4 条');
+                        this.cancelKey = data.cid;
+                        const checkKeys = this.cidObjArr.map(i => i.cid);
+                        this.$refs.tree.setCheckedKeys(checkKeys);
+                    }
+                } else { // 如果取消选择
+                    // 找到取消选择的下标
+                    const index = _.findIndex(this.cidObjArr, item => item.cid === data.cid);
+                    this.cidObjArr.splice(index, 1);
                 }
             },
-            handleSearch(val) {
-                this.nodeArr = [];
-                this.nodeArr.push(val.cid);
-                this.loading = true;
-                this.val = val;
-                if(val.cid!=""){
-                    this.cid = val.cid;
-                }else{
-                    this.getTree();
-                    this.getProgress();
-                }
-                setTimeout(() => {		       
-                    this.loading = false;
-                }, 1000);
-                
-            },
-            handleNodeClick(data) {
-                this.$refs.child.parentMsg(this.post);
-                if(data.children != undefined) {
-                    this.cid = data.cid;
-                    this.loading = true;
-                   
-                    setTimeout(() => {
-                        this.loading = false;
-                    }, 1000);
-                }
-
-            },
-            handleCheckChange() {
+            warn(msg) {
+                this.$message({
+                    message: msg,
+                    type: 'warning'
+                });
             },
             clickIndex(i, idx) {
                 this[`index${i}`] = idx;
