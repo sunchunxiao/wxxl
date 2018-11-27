077210012975c4ef1518d54dc521475a2f2f7d2c
zhangyao <823237853@qq.com>
2018-11-27 23:52:22 +0800

    feat(dateRecord): 时间记录

--- src/store/getters/product.js
+++ src/store/getters/product.js
@@ -1,13 +1,13 @@
 const product = {
-    productTree: state => state.product.tree,
-    progressArr: state => state.product.progressArr,
-    trendArr: state => state.product.trendArr,
-    rankArr: state => state.product.rankArr,
-    structureArr: state => state.product.structureArr,
-    compareArr: state => state.product.compareArr,
-    historyArr: state => state.product.historyArr,
-    strategyArr: state => state.product.strategyArr,
-    productDateArr: state => state.product.productDateArr,
+  productTree: state => state.product.tree,
+  progressArr: state => state.product.progressArr,
+  trendArr: state => state.product.trendArr,
+  rankArr: state => state.product.rankArr,
+  structureArr: state => state.product.structureArr,
+  compareArr: state => state.product.compareArr,
+  historyArr: state => state.product.historyArr,
+  strategyArr: state => state.product.strategyArr,
+  productDateArr: state => state.product.productDateArr
 };
 
 export default product;
