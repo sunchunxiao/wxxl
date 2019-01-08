1081c73a842ce8751125f576e70ae0fe798881f3
fengkairong <1281900421@qq.com>
2019-01-08 15:04:48 +0800

    fix 产品效率-全景分析cid未变,时间改变点击确认未发请求

--- src/views/Product/Trend.vue
+++ src/views/Product/Trend.vue
@@ -81,6 +81,9 @@ export default {
                 this.allRequest();
             },
             immediate: true
+        },
+        val() {
+            this.allRequest();
         }
     },
     methods: {
