a5e6d550f6a0ffb3c09fe09b6b14593ddb53b97b
fengkairong <1281900421@qq.com>
2019-01-11 17:28:08 +0800

    删除全景分析下目标实际差异分析clickIndex

--- src/views/Customer/OverviewMenu/Diff.vue
+++ src/views/Customer/OverviewMenu/Diff.vue
@@ -15,8 +15,7 @@
               <template v-for="(item, index) in custrendArr">
                 <el-col
                   :key="index"
-                  :span="12"
-                  @click.native="clickIndex(1 ,index)">
+                  :span="12">
                   <ProTargetActualDiffTrend
                     v-if="custrendArr.length"
                     :id="`${index}`"
--- src/views/Organization/OverviewMenu/Diff.vue
+++ src/views/Organization/OverviewMenu/Diff.vue
@@ -15,8 +15,7 @@
               <template v-for="(item, index) in orgtrendArr">
                 <el-col
                   :key="index"
-                  :span="12"
-                  @click.native="clickIndex(1 ,index)">
+                  :span="12">
                   <ProTargetActualDiffTrend
                     v-if="orgtrendArr.length"
                     :id="`${index}`"
