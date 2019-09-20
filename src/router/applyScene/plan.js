4687e3e1cb8565fd51c5c60525e103e30b059065
孙春晓 <15537791297@163.com>
2019-09-20 11:03:59 +0800

    Goodsplan

--- src/router/applyScene/plan.js
+++ src/router/applyScene/plan.js
@@ -5,6 +5,6 @@ export default [
         meta: {
             path: ['三大应用场景','计划', '货品计划']
         },
-        component: () => import('views/Plan/Overview.vue')
+        component: () => import('views/GoodsPlan/Overview.vue')
     },
 ];
